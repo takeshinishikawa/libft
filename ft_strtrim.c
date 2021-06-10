f7a7c6e8ac2f2ba818f2b0642539c0494e986d18
takeshinishikawa <takeshinishikawa@gmail.com>
2021-06-10 15:58:22 -0300

    fixes

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	len = ft_strlen(s1);
 	if (s1 == NULL || set == NULL)
-		return (NULL);
+		return (0);
 	while (*s1 && ft_strchr(set, *s1))
-	{
 		s1++;
-		len--;
-	}
 	while (len && ft_strchr(set, s1[len]))
 		len--;
 	return (ft_substr(s1, 0, len + 1));
