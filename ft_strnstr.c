850b126f9f4f9676c9f5c2b83a33e4481d0a7483
takeshinishikawa <takeshinishikawa@gmail.com>
2021-06-10 14:03:45 -0300

    strnstr fix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -7,19 +7,17 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	aux;
 
 	h = 0;
-	n = ft_strlen(needle);
+	n = ft_strlen(char *)needle);
 	if (n == 0)
 		return ((char *)haystack);
-	if (ft_strlen(needle) > ft_strlen(haystack))
-		return (NULL);
 	while (haystack[h] && h < len)
 	{
 		aux = 0;
-		while ((haystack[h + aux] == needle[n]) && ((h + aux) < len))
+		while ((haystack[h + aux] == needle[aux]) && ((h + aux) < len))
 		{
 			if (aux == n - 1)
 				return ((char *)haystack + h);
-			n++;
+			aux++;
 		}
 		h++;
 	}
