38f567f9ed79523e6ebafc8952004748d43f68eb
takeshinishikawa <takeshinishikawa@gmail.com>
2021-06-10 15:49:38 -0300

    fixes

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,6 +6,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*strjoin;
 
 	counter = 0;
+	if (!s1 || !s2)
+		return (NULL);
 	strjoin = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (strjoin == NULL)
 		return (NULL);
