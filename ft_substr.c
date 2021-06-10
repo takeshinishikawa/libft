8e6d94c685ff0ab444aa4c75447f9e2661af5404
takeshinishikawa <takeshinishikawa@gmail.com>
2021-06-10 15:18:24 -0300

    fixes

--- ft_substr.c
+++ ft_substr.c
@@ -5,15 +5,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*substr;
 	size_t	counter;
 	size_t	needle;
+	size_t	s_size;
 
 	counter = 0;
 	needle = start;
 	if (s == NULL)
 		return (NULL);
-	substr = (char *)malloc(len + 1);
+	s_size = ft_strlen(s);
+	substr = (char *)malloc(s_size + 1);
 	if (substr == NULL)
 		return (NULL);
-	while (counter < ft_strlen(s) && counter < len)
+	while (start < s_size && counter < ft_strlen(s) && counter < len)
 	{
 		substr[counter] = s[needle];
 		counter++;
